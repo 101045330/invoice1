bc3306633a738eb41416129ec20cc13be87cac15
101045330 <101045330@georgebrown.ca>
2025-03-23 01:36:57 +0000

    work hour list and subtotal of hours worked calculation accomplished

--- invoice-app/src/app/item1/item1.component.ts
+++ invoice-app/src/app/item1/item1.component.ts
@@ -7,12 +7,42 @@ import { CommonModule } from '@angular/common';
   templateUrl: './item1.component.html',
   styleUrl: './item1.component.scss'
 })
+
 export class Item1Component {
   @Input() mgInvoiceItems: any[] = [];
 
   subtotal: number = 0;
 
+  ngOnInit(): void {
+    this.calculateSubtotal();
+  }
+
+  calculateSubtotal(): void {
+    this.subtotal = this.mgInvoiceItems.reduce((total, mgEachInvoiceItems) => (total + mgEachInvoiceItems.mg_hours) , 0);
+  }
+
+ /* subtotal: number = 0;
+
   calculateSubtotal(input: number) {
     this.subtotal += input;
+  }*/
+
+/*
+  subtotal: number = 0;
+
+  ngOnInit(): void {
+    this.calculateSubtotal();
   }
+
+  calculateSubtotal(): void {
+    this.subtotal = this.mgInvoiceItems.reduce((mg_hours, img_hourly_rate) => mg_hours + img_hourly_rate, 0);
+  }
+
+  items: { name: string; price: number }[] = [
+    { name: 'Item 1', price: 10 },
+    { name: 'Item 2', price: 20 },
+    { name: 'Item 3', price: 30 }
+  ];
+  */
+
 }
