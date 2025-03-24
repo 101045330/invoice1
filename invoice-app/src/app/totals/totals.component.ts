f05b67ab37b8a31ef663d84987f5ec13965bfaac
101045330 <101045330@georgebrown.ca>
2025-03-24 01:14:28 +0000

    currency pipe added

--- invoice-app/src/app/totals/totals.component.ts
+++ invoice-app/src/app/totals/totals.component.ts
@@ -1,8 +1,9 @@
 import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-totals',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './totals.component.html',
   styleUrls: ['./totals.component.scss']
 })
@@ -22,7 +23,6 @@ export class TotalsComponent implements OnChanges {
     }
   }
 
-
   calculateTotals(): void {
     this.mg_subtotal = this.mgInvoiceItems.reduce((acc, item) => {
       const hours = Number(item.mg_hours);
@@ -37,17 +37,4 @@ export class TotalsComponent implements OnChanges {
     this.mg_total = +(this.mg_subtotal + this.mg_tax).toFixed(2);
     console.log("Totals Component: Subtotal: ", this.mg_subtotal, "Tax: ", this.mg_tax, "Total: ", this.mg_total);
   }
-  /**  calculateTotals1(): void {
-    this.mg_subtotal = this.mgInvoiceItems.reduce((acc, item) => {
-      const hours = Number(item.mg_hours);
-      const rate = Number(item.mg_hourly_rate); // Corrected line
-
-      if (isNaN(hours) || isNaN(rate)) {
-        return acc; // Return accumulator without adding if either value is NaN
-      }
-      return acc + (hours * rate);
-    }, 0);
-    this.mg_tax = +(this.mg_subtotal * (this.mg_taxRate / 100)).toFixed(2);
-    this.mg_total = +(this.mg_subtotal + this.mg_tax).toFixed(2);
-  } */
 }
