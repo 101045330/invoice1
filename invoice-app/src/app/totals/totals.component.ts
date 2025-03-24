99701f3d74f292bbf401c0b050a1e204c027bc1a
101045330 <101045330@georgebrown.ca>
2025-03-24 00:18:42 +0000

    totals and subtotals

--- invoice-app/src/app/totals/totals.component.ts
+++ invoice-app/src/app/totals/totals.component.ts
@@ -1,20 +1,19 @@
 import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
-
 @Component({
   selector: 'app-totals',
   imports: [],
   templateUrl: './totals.component.html',
-  styleUrl: './totals.component.scss'
+  styleUrls: ['./totals.component.scss']
 })
 export class TotalsComponent implements OnChanges {
-
   @Input() mgInvoiceItems: any[] = [];
+   mg_hours: number = 0;
 
-  //Local properties
-  mg_subtotal: number = 0;
+  mg_subtotal: number = 0; 
+  mg_taxRate: number = 13;
   mg_tax: number = 0;
   mg_total: number = 0;
-  mg_taxRate: number = 13;
+ 
 
   ngOnChanges(changes: SimpleChanges): void {
     if (changes['mgInvoiceItems']) {
@@ -22,34 +21,16 @@ export class TotalsComponent implements OnChanges {
     }
   }
   calculateTotals(): void {
-    this.mg_subtotal = this.mgInvoiceItems.reduce((sum, item) => {
-      return sum + item.mgRate * item.mgHours;
+    this.mg_subtotal = this.mgInvoiceItems.reduce((acc, item) => {
+      const hours = Number(item.mg_hours);
+      const rate = Number(item.mg_taxRate);
+
+      if (isNaN(hours) || isNaN(rate)) { return acc + (hours * rate); }
+      return acc;
     }, 0);
     this.mg_tax = +(this.mg_subtotal * (this.mg_taxRate / 100)).toFixed(2);
     this.mg_total = +(this.mg_subtotal + this.mg_tax).toFixed(2);
   }
-
 }
-/*
-@Input() mgItems:{mgName: string, mgHours: number, mgRate: number}[] = [];
-@Input() mgTax: number = 0.13;
- 
-//compute the total hours worked
-get mgTotalHours(): number {
-  return this.mgItems.reduce((acc, item) => acc + item.mgHours, 0);
-}
- 
-get mgSubtotal():number{
-  return this.mgItems.reduce((sum, item) => sum + item.mgRate * item.mgHours, 0);
-}
- 
-get mgTaxAmount():number{
-  return this.mgSubtotal * this.mgTax;
-}
- 
-get mgTotal():number{
-  return this.mgSubtotal + this.mgTaxAmount
-}
-*/
 
 
